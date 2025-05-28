a3f76478868dc5a59118d9c56a11705e27c4ed31
yunei0313 <yunei0313@gmail.com>
2025-05-28 19:54:55 +0900

    코드 스타일 적용

--- src/markdown-it-image-plugin.ts
+++ src/markdown-it-image-plugin.ts
@@ -8,8 +8,8 @@ const tokenType = "ob_img";
 
 export interface MarkdownItImageActionParams
 {
-    src : string;
-    width? : string;
+    src     : string;
+    width?  : string;
     height? : string;
 }
 
@@ -33,75 +33,90 @@ export const MarkdownItImagePluginInstance = {
     }
 };
 
-function plugin( md : MarkdownIt ) : void
-{
-    md.inline.ruler.after( "image", tokenType, ( state, silent ) =>
-    {
-        const regex = /^!\[\[([^|\]\n]+)(\|([^\]\n]+))?\]\]/;
-        const match = state.src.slice( state.pos )
-        .match( regex );
-        if ( match )
-        {
-            if ( silent )
-            {
-                return true;
-            }
-            const token = state.push( tokenType, "img", 0 );
-            const matched = match[ 0 ];
-            const src = match[ 1 ];
-            const size = match[ 3 ];
-            let width : string | undefined;
-            let height : string | undefined;
-            if ( size )
-            {
-                const sepIndex = size.indexOf( "x" ); // width x height
-                if ( sepIndex > 0 )
-                {
-                    width = trim( size.substring( 0, sepIndex ) );
-                    height = trim( size.substring( sepIndex + 1 ) );
-                    token.attrs = [ [ "src", src ], [ "width", width ], [ "height", height ] ];
-                }
-                else
-                {
-                    width = trim( size );
-                    token.attrs = [ [ "src", src ], [ "width", width ] ];
-                }
-            }
-            else
-            {
-                token.attrs = [ [ "src", src ] ];
-            }
-            if ( pluginOptions.doWithImage )
-            {
-                pluginOptions.doWithImage( {
-                    src : token.attrs?.[ 0 ]?.[ 1 ], width : token.attrs?.[ 1 ]?.[ 1 ], height : token.attrs?.[ 2 ]?.[ 1 ]
-                } );
-            }
-            state.pos += matched.length;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    } );
-    md.renderer.rules.ob_img = ( tokens : Token[], idx : number ) =>
-    {
-        const token = tokens[ idx ];
-        const src = token.attrs?.[ 0 ]?.[ 1 ];
-        const width = token.attrs?.[ 1 ]?.[ 1 ];
-        const height = token.attrs?.[ 2 ]?.[ 1 ];
-        if ( width )
-        {
-            if ( height )
-            {
-                return `<img src="${ src }" width="${ width }" height="${ height }" alt="">`;
-            }
-            return `<img src="${ src }" width="${ width }" alt="">`;
-        }
-        else
-        {
-            return `<img src="${ src }" alt="">`;
-        }
-    };
+function plugin( md : MarkdownIt ) : void  
+{  
+    md.inline.ruler.after( "image", tokenType, ( state, silent ) =>  
+    {  
+        const regex = /^!\[\[([^|\]\n]+)(\|([^\]\n]+))?\]\]/;  
+        const match = state.src.slice( state.pos ).match( regex );  
+        
+        if ( match )  
+        {  
+            if ( silent )  
+            {  
+                return true;  
+            }  
+            
+            const token = state.push( tokenType, "img", 0 );  
+            
+            const matched = match[ 0 ];  
+            const src     = match[ 1 ];  
+            const size    = match[ 3 ];  
+            
+            let width  : string | undefined;  
+            let height : string | undefined;  
+            
+            if ( size )  
+            {  
+                const sepIndex = size.indexOf( "x" ); // width x height  
+                
+                if ( sepIndex > 0 )  
+                {  
+                    width  = trim( size.substring( 0, sepIndex ) );  
+                    height = trim( size.substring( sepIndex + 1 ) );  
+                    
+                    token.attrs = [ [ "src", src ], [ "width", width ], [ "height", height ] ];  
+                }  
+                else  
+                {  
+                    width = trim( size );  
+                    
+                    token.attrs = [ [ "src", src ], [ "width", width ] ];  
+                }  
+            }  
+            else  
+            {  
+                token.attrs = [ [ "src", src ] ];  
+            }  
+            if ( pluginOptions.doWithImage )  
+            {  
+                pluginOptions.doWithImage
+                ( {  
+                    src    : token.attrs?.[ 0 ]?.[ 1 ], 
+                    width  : token.attrs?.[ 1 ]?.[ 1 ], 
+                    height : token.attrs?.[ 2 ]?.[ 1 ]  
+                } );  
+            }  
+            
+            state.pos += matched.length;  
+            
+            return true;  
+        }  
+        else  
+        {  
+            return false;  
+        }  
+    } );  
+    
+    md.renderer.rules.ob_img = ( tokens : Token[], idx : number ) =>  
+    {  
+        const token = tokens[ idx ];  
+        
+        const src    = token.attrs?.[ 0 ]?.[ 1 ];  
+        const width  = token.attrs?.[ 1 ]?.[ 1 ];  
+        const height = token.attrs?.[ 2 ]?.[ 1 ];  
+        
+        if ( width )  
+        {  
+            if ( height )  
+            {  
+                return `<img src="${ src }" width="${ width }" height="${ height }" alt="">`;  
+            }  
+            return `<img src="${ src }" width="${ width }" alt="">`;  
+        }  
+        else  
+        {  
+            return `<img src="${ src }" alt="">`;  
+        }  
+    };  
 }
